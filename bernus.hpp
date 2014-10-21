4b8baefe3d70c0d4d8ea29422c402bf4469bab52
Daniel Ruprecht <daniel.ruprecht@usi.ch>
2014-10-21 15:06:15 +0200

    added calls to stationary gate functions to ion current computations; ode based gates are still missing

--- bernus.hpp
+++ bernus.hpp
@@ -9,9 +9,6 @@ class bernus: public Iionmodel {
 public:
   
   bernus() {
-    //TODO: Find out how to make this const and static but still a function of
-    // the other parameters...
-    this->test = bernus::R*bernus::T*bernus::Fa;
     
     //TODO: Somehow initialize gating variables
     for(int i=0; i<this->ngates; i++){
@@ -20,6 +17,13 @@ public:
     
     // Initialize bernus function object
     this->_bnf = new bernus_functions();
+    
+    //TODO: Efficient calculation of equilibrium potentials from constants
+    this->_e_na = 0.0*bernus::R*bernus::T;
+    this->_e_ca = 0.0*bernus::R;
+    this->_e_k  = 0.0*bernus::R;
+    this->_e_to = 0.0*bernus::R;
+    
   };
   
   double ionforcing(double);
@@ -78,15 +82,15 @@ private:
    * TODO: Check physical units...
    */
   //TODO: Should make precision here choosable as well
-  double static const g_na   = 16.0;
-  double static const g_ca   = 0.064;
-  double static const g_to   = 0.4;
-  double static const g_k    = 0.019;
-  double static const g_k1   = 3.9;
-  double static const g_na_b = 0.001;
-  double static const g_ca_b = 0.00085;
-  double static const g_nak  = 1.3;
-  double static const g_naca = 1.0;
+  double static const _g_na   = 16.0;
+  double static const _g_ca   = 0.064;
+  double static const _g_to   = 0.4;
+  double static const _g_k    = 0.019;
+  double static const _g_k1   = 3.9;
+  double static const _g_na_b = 0.001;
+  double static const _g_ca_b = 0.00085;
+  double static const _g_nak  = 1.3;
+  double static const _g_naca = 1.0;
   
   //! Universal gas constant
   double static const R = 0.0; //TODO: Correct value
@@ -100,7 +104,7 @@ private:
   //! Extra- and inner cellular potentials (Table 1 in Bernus et al.)
   
   //! Equilibrium potentials
-  double test;
+  double _e_na, _e_ca, _e_to, _e_k;
   
 };
 #endif // BERNUS
@@ -121,32 +125,36 @@ inline double bernus::ionforcing(double V)
 
  //! Sodium current i_Na
 inline double bernus::i_na(double V)
-{return 0.0;}
+{return _g_na*(V-_e_na);} //TODO: add ODE-based gates m, v
 
 //! Calcium current i_Ca
 inline double bernus::i_ca(double V)
-{return _bnf->d_inf(V)*_bnf->f_ca(V);}
+{return _g_ca*_bnf->d_inf(V)*_bnf->f_ca(V)*(V-_e_ca);} //TODO: add ODE-based gate f
 
 //! Transient outward current i_to
 inline double bernus::i_to(double V)
-{return _bnf->r_inf(V);}
+{return _g_k*_bnf->r_inf(V)*(V-_e_to);} //TODO: add ODE-based gate to
 
 //! Delated rectifier potassium current i_K
 inline double bernus::i_k(double V)
-{return 0.0;}
+{return _g_k*(V-_e_k);} //TODO: add ODE gate X
 
 //! Inward rectifier potassium current i_K1
 inline double bernus::i_k1(double V)
-{return _bnf->k1_inf(V);}
+{return _g_k1*_bnf->k1_inf(V)*(V-_e_k);}
 
+//! Calcium background current
 inline double bernus::i_b_ca(double V)
-{return 0.0;}
+{return _g_ca_b*(V-_e_ca);}
 
+//! Sodium background current
 inline double bernus::i_b_na(double V)
-{return 0.0;}
+{return _g_na_b*(V - _e_na);}
 
+//! Sodium potassium pump
 inline double bernus::i_na_k(double V)
-{return _bnf->f_nak(V)*_bnf->f_nak_a(V);}
+{return _g_nak*_bnf->f_nak(V)*_bnf->f_nak_a(V);}
 
+//! Sodium calcium pump
 inline double bernus::i_na_ca(double V)
-{return _bnf->f_naca(V);}
+{return _g_naca*_bnf->f_naca(V);}
